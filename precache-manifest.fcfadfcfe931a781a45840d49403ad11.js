f1c234adc764f0b57238aa89dab4c79f9d09a2cb
jcnimble <jcginelli@outlook.com>
2019-05-28 20:10:35 -0400

    Updates

---
+++ precache-manifest.fcfadfcfe931a781a45840d49403ad11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341b142d75488b594b97b9c032358922",
+    "url": "/jcnimble/clicky-game.git/index.html"
+  },
+  {
+    "revision": "de3aa9f9362d69c777cd",
+    "url": "/jcnimble/clicky-game.git/static/css/main.d1009e63.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/jcnimble/clicky-game.git/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "de3aa9f9362d69c777cd",
+    "url": "/jcnimble/clicky-game.git/static/js/main.51d2870f.chunk.js"
+  },
+  {
+    "revision": "63beea00756b6e3ab3ce",
+    "url": "/jcnimble/clicky-game.git/static/js/runtime~main.affe97f9.js"
+  }
+]);
